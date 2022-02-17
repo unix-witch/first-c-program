eb96b6349c82dcda18bb89f60e387e0df09888fd
bsd_witch <77200446+unix-witch@users.noreply.github.com>
2022-02-16 21:45:28 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int run = 1;
+    int items;
+    int total_cost;
+    int cost;
+    int type
+    char income[256];
+    fgets(income, 256, stdin);
+    fgets(items, 256, stdin);
+    int i = 0;
+    int cost = 1;
+    int total_cost = 1
+    for (i <= items) {
+        printf("Enter in Cost of item\n");
+        scanf(">%d", cost);
+        printf("\nWas this a food item?\n1. Yes\n2. No\n");
+        scanf(">%d", type);
+        printf("\n")
+        while (type < 0 && > 2){
+            printf("Enter in a valid option");
+            scanf(">%d", type);
+        }
+        if (type == 1){
+        } else {
+            int total_cost = cost * 1.06
+            
+        }
+        
+    
+    }
+    return 0;
+}
+
+
